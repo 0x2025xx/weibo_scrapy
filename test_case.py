8915408024582a2ef2b21f5153e2108acd64db44
rob0x2025 <rob0x2025@gmail.com>
2024-07-05 13:44:58 -1000

    reformat code in utf8

--- test_case.py
+++ test_case.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 
 
 def get_douban_info():
@@ -8,24 +8,23 @@ def get_douban_info():
     url = 'https://movie.douban.com/top250?start='
 
     headers = {
-        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
-    #return
+
+    # return
 
     def fetch(session, page):
-        with (session.get(f'{url}{page*25}', headers=headers) as r,
-            open(f'./douban_movie_html/top250-{page}.html', 'w') as f):
+        with (session.get(f'{url}{page * 25}', headers=headers) as r,
+              open(f'./douban_movie_html/top250-{page}.html', 'w') as f):
             f.write(r.text)
 
     def run_task():
-
         with requests.Session() as session:
             for p in range(25):
                 fetch(session, p)
         return
 
     def run_task2():
-
         from multiprocessing import Pool
 
         with requests.Session() as session:
@@ -40,54 +39,13 @@ def get_douban_info():
         with (Pool() as pool, requests.Session() as session):
             pool.starmap(fetch_proj, data)
 
-            
-
         return
 
     # run_task()
     # run_task2()
 
 
-
 if __name__ == '__main__':
-
-    
     get_douban_info()
 
-
-
-
-
-
-
     print('=end=')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- test_py3_object_.py
+++ test_py3_object_.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-#coding:utf-8
-
-
-
-
+# coding:utf-8
 
 
 class MyFirstClass:
@@ -13,12 +9,11 @@ class MyFirstClass:
     pass
 
 
-
 class Point:
     def reset(self):
         self.x = 0
         self.y = 0
-    
+
     def reset2(this):
         this.x = 0
         this.y = 0
@@ -26,7 +21,9 @@ class Point:
     def reset3(marcha):
         marcha.x = 0
         marcha.y = 0
+
     pass
+
 
 # p1 = Point()
 
@@ -42,8 +39,50 @@ class Point:
 # print(p1.x, p1.y)
 # print(p2.x, p2.y)
 
-p = Point()
+# p = Point()
 
-p.reset3()
+# p.reset3()
 
-print(p.x, p.y)
+# print(p.x, p.y)
+
+# p = Point()
+
+# Point.reset(p)
+
+# print(p.x, p.y)
+
+import math
+
+
+class Point:
+
+    def __init__(self, x=None, y=None):
+        self.move(x, y)
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    def reset(self):
+        self.move(0, 0)
+
+    def calculate_distance(self, other_point):
+        data = math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
+        return '%6.3f' % data
+
+
+point1 = Point()
+point2 = Point()
+
+point1.reset()
+point2.move(5, 0)
+
+assert (point2.calculate_distance(point1) == point1.calculate_distance(point2))
+point1.move(3, 4)
+
+print(point1.calculate_distance(point2))
+print(point1.calculate_distance(point1))
+
+point = Point(3, 5)
+print(point.x)
+print(point.y)
