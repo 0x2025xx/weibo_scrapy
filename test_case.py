ded2f355754d51a4a850534d6076995b7fd0dfd2
rob0x2025 <rob0x2025@gmail.com>
2024-07-04 16:30:11 -1000

    add method to get douban movie info

--- test_case.py
+++ test_case.py
@@ -1,26 +1,42 @@
 #coding:utf-8
 
 
+def get_douban_info():
+    #
+    import requests
 
+    url = 'https://movie.douban.com/top250?start='
 
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+    }
+    #return
 
+    def fetch(session, page):
+        with (session.get(f'{url}{page*25}', headers=headers) as r, 
+            open(f'top250-{page}.html', 'w') as f):
+            f.write(r.text)
 
+    def run_task():
 
+        with requests.Session() as session:
+            for p in range(25):
+                fetch(session, p)
+        return
 
-
-
-
-
-
-
-
-
-
-
+    run_task()
 
 
 
 if __name__ == '__main__':
+
+    get_douban_info()
+
+
+
+
+
+
 
     print('=end=')
 
