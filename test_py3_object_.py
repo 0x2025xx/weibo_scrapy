5ab813cf78e4824f7592d0c864c470938c68ed93
rob0x2025 <rob0x2025@gmail.com>
2024-07-05 22:53:06 -1000

    1 add comment 2 add a method to clean win system

--- test_py3_object_.py
+++ test_py3_object_.py
@@ -55,34 +55,43 @@ import math
 
 
 class Point:
+    'Represnets a point in two-dimentionals geometric coordinates'
 
     def __init__(self, x=None, y=None):
+        '''
+        Initialize the position of a new point.
+        the x and y coordiantes can be specified
+        if not the point defaults to origin.
+        '''
         self.move(x, y)
 
     def move(self, x, y):
+        'move the point to a new place of a 2d point'
         self.x = x
         self.y = y
 
     def reset(self):
+        'reset the point back to the geometric origin:0,0'
         self.move(0, 0)
 
     def calculate_distance(self, other_point):
+        'caculate thr distance from this point to a second point passed as a parameter ,the distance returns a float'
         data = math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
         return '%6.3f' % data
 
 
-point1 = Point()
-point2 = Point()
+# point1 = Point()
+# point2 = Point()
 
-point1.reset()
-point2.move(5, 0)
+# point1.reset()
+# point2.move(5, 0)
 
-assert (point2.calculate_distance(point1) == point1.calculate_distance(point2))
-point1.move(3, 4)
+# assert (point2.calculate_distance(point1) == point1.calculate_distance(point2))
+# point1.move(3, 4)
 
-print(point1.calculate_distance(point2))
-print(point1.calculate_distance(point1))
+# print(point1.calculate_distance(point2))
+# print(point1.calculate_distance(point1))
 
-point = Point(3, 5)
-print(point.x)
-print(point.y)
+# point = Point(3, 5)
+# print(point.x)
+# print(point.y)
