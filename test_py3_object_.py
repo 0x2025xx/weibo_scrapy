da87cf46a846a421ecd69db3cfb5faefa8a6b65f
rob0x2025 <rob0x2025@gmail.com>
2024-07-08 23:51:12 -1000

    test function variable not useable

--- test_py3_object_.py
+++ test_py3_object_.py
@@ -95,3 +95,60 @@ class Point:
 # point = Point(3, 5)
 # print(point.x)
 # print(point.y)
+
+def format_string(string, formatter=None):
+    '''
+    format s string using the formatter object,
+    which is expected to have a format method that accepts a string.
+    '''
+    class DefaultFormatter:
+        '''
+        format s string in title case. 
+        '''
+        def format(self,string):
+            return str(string).title()
+            pass
+        pass
+    pass
+
+    if not formatter:
+        formatter = DefaultFormatter()
+        return formatter.format(string)
+
+# hello_string = "hello world, how are you today."
+# print("input: " + hello_string)
+
+# print("output: " + format_string(hello_string))
+
+
+class SecretString:
+    '''
+    a not-at-all secure way to store a secret string.
+    '''
+
+    def __init__(self, plain_string, pass_phrase):
+        self.__plain_string = plain_string
+        self.__pass_phrase = pass_phrase
+        pass
+
+    def decrypt(self, pass_phrase):
+        '''
+        only show the string if the pass_phrase is correct
+        '''
+
+        if pass_phrase == self.__pass_phrase:
+            return self.__plain_string
+        else:
+            return ''
+        pass
+
+
+
+
+
+
+
+
+
+
+
